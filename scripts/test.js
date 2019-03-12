cd2107a2f418995cb72f4f545312b4e41a2aaa69
Dean Haleem <deanhaleem.10@gmail.com>
2019-03-11 23:33:33 -0400

    clean up files

--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-      if($(this).scrollTop() > 200) { 
-          $('.navbar').addClass('solid');
-      } else {
-          $('.navbar').removeClass('solid');
-      }
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.navbar').addClass('solid');
+        } else {
+            $('.navbar').removeClass('solid');
+        }
     });
 });
